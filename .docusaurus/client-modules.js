9e3bd2f37f99c4ec1a0190f6e2987beedc797fc5
Carlos Dias <cesdias@gsma.com>
2022-08-08 14:25:47 -0300

    removed content generated automatically

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/eanjos/Documents/Dev/token/token-docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/eanjos/Documents/Dev/token/token-docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/eanjos/Documents/Dev/token/token-docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/eanjos/Documents/Dev/token/token-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/eanjos/Documents/Dev/token/token-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/eanjos/Documents/Dev/token/token-docs/src/frontend/custom.css"),
-];
